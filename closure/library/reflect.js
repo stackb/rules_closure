c8508a661805d60c5710530e06956f328dd11747
Goktug Gokdogan <goktug@google.com>
2025-01-23 14:58:38 -0800

    Update copy of reflect.js with the one from Closure Compiler (#653)

--- closure/library/reflect.js
+++ closure/library/reflect.js
@@ -1,11 +1,12 @@
 /**
  * @license
  * Copyright The Closure Library Authors.
+ * Copyright The Closure Compiler Authors.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 /**
- * @fileoverview Useful compiler idioms.
+ * @fileoverview Useful compiler idioms related to renaming.
  */
 
 goog.provide('goog.reflect');
@@ -13,24 +14,23 @@ goog.provide('goog.reflect');
 
 /**
  * Syntax for object literal casts.
- * @see http://go/jscompiler-renaming
  * @see https://goo.gl/CRs09P
  *
  * Use this if you have an object literal whose keys need to have the same names
  * as the properties of some class even after they are renamed by the compiler.
  *
  * @param {!Function} type Type to cast to.
- * @param {Object} object Object literal to cast.
- * @return {Object} The object literal.
+ * @param {!Object} object Object literal to cast.
+ * @return {!Object} The object literal.
  */
 goog.reflect.object = function(type, object) {
   'use strict';
   return object;
 };
 
+
 /**
  * Syntax for renaming property strings.
- * @see http://go/jscompiler-renaming
  * @see https://goo.gl/CRs09P
  *
  * Use this if you have an need to access a property as a string, but want
@@ -49,13 +49,15 @@ goog.reflect.objectProperty = function(prop, object) {
   return prop;
 };
 
+
 /**
  * To assert to the compiler that an operation is needed when it would
  * otherwise be stripped. For example:
- * <code>
- *     // Force a layout
- *     goog.reflect.sinkValue(dialog.offsetHeight);
- * </code>
+ *
+ * ```javascript
+ * // Force a layout
+ * goog.reflect.sinkValue(dialog.offsetHeight);
+ * ```
  * @param {T} x
  * @return {T}
  * @template T
@@ -72,61 +74,3 @@ goog.reflect.sinkValue = function(x) {
  * goog.reflect.sinkValue.
  */
 goog.reflect.sinkValue[' '] = function() {};
-
-
-/**
- * Check if a property can be accessed without throwing an exception.
- * @param {Object} obj The owner of the property.
- * @param {string} prop The property name.
- * @return {boolean} Whether the property is accessible. Will also return true
- *     if obj is null.
- */
-goog.reflect.canAccessProperty = function(obj, prop) {
-  'use strict';
-  try {
-    goog.reflect.sinkValue(obj[prop]);
-    return true;
-  } catch (e) {
-  }
-  return false;
-};
-
-
-/**
- * Retrieves a value from a cache given a key. The compiler provides special
- * consideration for this call such that it is generally considered side-effect
- * free. However, if the `opt_keyFn` or `valueFn` have side-effects
- * then the entire call is considered to have side-effects.
- *
- * Conventionally storing the value on the cache would be considered a
- * side-effect and preclude unused calls from being pruned, ie. even if
- * the value was never used, it would still always be stored in the cache.
- *
- * Providing a side-effect free `valueFn` and `opt_keyFn`
- * allows unused calls to `goog.reflect.cache` to be pruned.
- *
- * @param {!Object<K, V>} cacheObj The object that contains the cached values.
- * @param {?} key The key to lookup in the cache. If it is not string or number
- *     then a `opt_keyFn` should be provided. The key is also used as the
- *     parameter to the `valueFn`.
- * @param {function(?):V} valueFn The value provider to use to calculate the
- *     value to store in the cache. This function should be side-effect free
- *     to take advantage of the optimization.
- * @param {function(?):K=} opt_keyFn The key provider to determine the cache
- *     map key. This should be used if the given key is not a string or number.
- *     If not provided then the given key is used. This function should be
- *     side-effect free to take advantage of the optimization.
- * @return {V} The cached or calculated value.
- * @template K
- * @template V
- */
-goog.reflect.cache = function(cacheObj, key, valueFn, opt_keyFn) {
-  'use strict';
-  const storedKey = opt_keyFn ? opt_keyFn(key) : key;
-
-  if (Object.prototype.hasOwnProperty.call(cacheObj, storedKey)) {
-    return cacheObj[storedKey];
-  }
-
-  return (cacheObj[storedKey] = valueFn(key));
-};
